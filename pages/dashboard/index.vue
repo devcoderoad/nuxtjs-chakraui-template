3fb96e15e4bbd4d8d9c61da05507cf4bc2a0f8bd
devcoderoad <devdef20@gmail.com>
2022-10-09 12:15:54 +0700

    chore: update assets

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,14 +1,206 @@
 <template>
-  <CBox as="article">
-    <!-- <h1 v-chakra :class="style" color="brand.100">Dashboard Page</h1> -->
-    <h1 v-chakra color="brand.100" fontWeight="black">Dashboard Page</h1>
-    <h2 v-chakra color="brand.300" fontWeight="black">Dashboard Page</h2>
-    <h3 v-chakra color="brand.600" fontWeight="black">Dashboard Page</h3>
-    <h4 v-chakra color="brand.900" fontWeight="black">Dashboard Page</h4>
-    <CHeading color="secondary.100" fontWeight="black">Dashboard Page</CHeading>
-    <CHeading color="secondary.300" fontWeight="black">Dashboard Page</CHeading>
-    <CHeading color="secondary.600" fontWeight="black">Dashboard Page</CHeading>
-    <CHeading color="secondary.900" fontWeight="black">Dashboard Page</CHeading>
+  <CBox as="article" mb="24">
+    <CBox mx="auto" mb="12" :display="['block', 'flex']">
+      <CBox :w="['100%', '33.3%']" pr="6" mt="3">
+        <CHeading>Welcome to Nuxtify</CHeading>
+        <CText as="p">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+          explicabo quisquam necessitatibus ut quae impedit, eaque nemo illo,
+          dignissimos architecto vitae atque cupiditate officiis placeat
+          quibusdam distinctio sint! Amet, ad, tempora, tempore consectetur
+          error optio delectus magni molestiae perferendis assumenda dolorum
+          fugiat sit minima.
+        </CText>
+      </CBox>
+      <CBox :w="['100%', '66.6%']" :px="[0, 6]">
+        <CSimpleGrid
+          :columns="[1, 2, 4]"
+          :spacing="['20px', '40px']"
+          mx="auto"
+          mt="6"
+          mb="24"
+        >
+          <CBox
+            :_hover="{ shadow: 'xl' }"
+            borderWidth="1px"
+            overflow="hidden"
+            shadow="lg"
+            p="10"
+            rounded="lg"
+            :maxW="['xxl', 'md']"
+            ><CImage
+              mb="8"
+              borderRadius="full"
+              borderWidth="12px"
+              borderColor="gray.200"
+              alt="NuxtCha"
+              src="https://images.unsplash.com/photo-1592329347431-0943ca753471?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=960&h=960&q=80"
+              fallbackSrc="https://via.placeholder.com/150"
+            />
+            <CBox d="flex" align-items="baseline">
+              <CBadge rounded="full" px="2" variant-color="green"> New </CBadge>
+              <CBox
+                color="gray.500"
+                font-weight="semibold"
+                letter-spacing="wide"
+                font-size="xs"
+                text-transform="uppercase"
+                ml="2"
+              >
+                3 beds &bull; 2 baths
+              </CBox>
+            </CBox>
+            <CBox
+              mt="1"
+              font-weight="semibold"
+              as="h4"
+              line-height="tight"
+              is-truncated
+            >
+              Modern home in city center in the heart of historic Los Angeles
+            </CBox>
+            <CBox>
+              $1,900.00 / wk
+              <CBox as="span" color="gray.600" fontSize="sm"> / wk </CBox>
+            </CBox>
+          </CBox>
+          <CBox
+            :_hover="{ shadow: 'xl' }"
+            borderWidth="1px"
+            overflow="hidden"
+            shadow="lg"
+            p="10"
+            rounded="lg"
+            :maxW="['xxl', 'md']"
+            ><CImage
+              mb="8"
+              borderRadius="full"
+              borderWidth="12px"
+              borderColor="gray.200"
+              alt="NuxtCha"
+              src="https://images.unsplash.com/photo-1592329347431-0943ca753471?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=960&h=960&q=80"
+              fallbackSrc="https://via.placeholder.com/150"
+            />
+            <CBox d="flex" align-items="baseline">
+              <CBadge rounded="full" px="2" variant-color="green"> New </CBadge>
+              <CBox
+                color="gray.500"
+                font-weight="semibold"
+                letter-spacing="wide"
+                font-size="xs"
+                text-transform="uppercase"
+                ml="2"
+              >
+                3 beds &bull; 2 baths
+              </CBox>
+            </CBox>
+            <CBox
+              mt="1"
+              font-weight="semibold"
+              as="h4"
+              line-height="tight"
+              is-truncated
+            >
+              Modern home in city center in the heart of historic Los Angeles
+            </CBox>
+            <CBox>
+              $1,900.00 / wk
+              <CBox as="span" color="gray.600" fontSize="sm"> / wk </CBox>
+            </CBox>
+          </CBox>
+          <CBox
+            :_hover="{ shadow: 'xl' }"
+            borderWidth="1px"
+            overflow="hidden"
+            shadow="lg"
+            p="10"
+            rounded="lg"
+            :maxW="['xxl', 'md']"
+          >
+            <CImage
+              mb="8"
+              borderRadius="full"
+              borderWidth="12px"
+              borderColor="gray.200"
+              alt="NuxtCha"
+              src="https://images.unsplash.com/photo-1592329347431-0943ca753471?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=960&h=960&q=80"
+              fallbackSrc="https://via.placeholder.com/150"
+            />
+            <CBox d="flex" align-items="baseline">
+              <CBadge rounded="full" px="2" variant-color="green"> New </CBadge>
+              <CBox
+                color="gray.500"
+                font-weight="semibold"
+                letter-spacing="wide"
+                font-size="xs"
+                text-transform="uppercase"
+                ml="2"
+              >
+                3 beds &bull; 2 baths
+              </CBox>
+            </CBox>
+            <CBox
+              mt="1"
+              font-weight="semibold"
+              as="h4"
+              line-height="tight"
+              is-truncated
+            >
+              Modern home in city center in the heart of historic Los Angeles
+            </CBox>
+            <CBox>
+              $1,900.00 / wk
+              <CBox as="span" color="gray.600" fontSize="sm"> / wk </CBox>
+            </CBox>
+          </CBox>
+          <CBox
+            :_hover="{ shadow: 'xl' }"
+            borderWidth="1px"
+            overflow="hidden"
+            shadow="lg"
+            p="10"
+            rounded="lg"
+            :maxW="['xxl', 'md']"
+          >
+            <CImage
+              mb="8"
+              borderRadius="full"
+              borderWidth="12px"
+              borderColor="gray.200"
+              alt="NuxtCha"
+              src="https://images.unsplash.com/photo-1592329347431-0943ca753471?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=960&h=960&q=80"
+              fallbackSrc="https://via.placeholder.com/150"
+            />
+            <CBox d="flex" align-items="baseline">
+              <CBadge rounded="full" px="2" variant-color="green"> New </CBadge>
+              <CBox
+                color="gray.500"
+                font-weight="semibold"
+                letter-spacing="wide"
+                font-size="xs"
+                text-transform="uppercase"
+                ml="2"
+              >
+                3 beds &bull; 2 baths
+              </CBox>
+            </CBox>
+            <CBox
+              mt="1"
+              font-weight="semibold"
+              as="h4"
+              line-height="tight"
+              is-truncated
+            >
+              Modern home in city center in the heart of historic Los Angeles
+            </CBox>
+            <CBox>
+              $1,900.00 / wk
+              <CBox as="span" color="gray.600" fontSize="sm"> / wk </CBox>
+            </CBox>
+          </CBox>
+        </CSimpleGrid>
+      </CBox>
+    </CBox>
     <Cbox>
       <CSimpleGrid
         :columns="[1, 2, 3, 4]"
@@ -216,7 +408,7 @@
 </template>
 <script>
 /* Chakra UI */
-import { CBox, CSimpleGrid, CHeading, CImage } from '@chakra-ui/vue'
+import { CBox, CSimpleGrid, /* CHeading, */ CImage } from '@chakra-ui/vue'
 /* Components */
 import MAlert from '~/components/alert/MAlert.vue'
 import MToast from '~/components/toast/MToast.vue'
@@ -226,7 +418,7 @@ export default {
   components: {
     CBox,
     CSimpleGrid,
-    CHeading,
+    // CHeading,
     CImage,
     MAlert,
     MToast,
--- pages/dashboard/paragraph.vue
+++ pages/dashboard/paragraph.vue
@@ -1,28 +1,61 @@
 <template>
   <CBox>
-    <CHeading color="brand.100">Heading 100 Brand</CHeading>
-    <CHeading color="brand.300">Heading 300 Brand</CHeading>
-    <CHeading color="brand.600">Heading 600 Brand</CHeading>
-    <CHeading color="brand.900">Heading 900 Brand</CHeading>
+    <CHeading>Paragraph</CHeading>
     <CDivider />
-    <CHeading color="secondary.100">Heading 100 Secondary</CHeading>
-    <CHeading color="secondary.300">Heading 300 Secondary</CHeading>
-    <CHeading color="secondary.600">Heading 600 Secondary</CHeading>
-    <CHeading color="secondary.900" textShadow="sm"
-      >Heading 900 Secondary</CHeading
-    >
+    <CStack :spacing="3">
+      <CText fontSize="6xl">In love with Vue and Nuxt</CText>
+      <CText fontSize="5xl">In love with Vue and Nuxt</CText>
+      <CText fontSize="4xl">In love with Vue and Nuxt</CText>
+      <CText fontSize="3xl">In love with Vue and Nuxt</CText>
+      <CText fontSize="2xl">In love with Vue and Nuxt</CText>
+      <CText fontSize="xl">In love with Vue and Nuxt</CText>
+      <CText fontSize="lg">In love with Vue and Nuxt</CText>
+      <CText fontSize="md">In love with Vue and Nuxt</CText>
+      <CText fontSize="sm">In love with Vue and Nuxt</CText>
+      <CText fontSize="xs">In love with Vue and Nuxt</CText>
+    </CStack>
     <CDivider />
-    Mode: <code>{{ chakraColorMode }}</code>
+    <CHeading>Truncate</CHeading>
+    <CDivider />
+    <CText color="gray.500" is-truncated>
+      “I was attracted to JavaScript because of the ability to just build
+      something and share it instantly with the world. You put it on the web,
+      and you get a URL, you can send it to anyone with a browser. That was the
+      part that just attracted me to the web and to JavaScript.”
+    </CText>
+    <CDivider />
+    <CHeading>Element</CHeading>
+    <CStack :spacing="3">
+      <CText as="i">Italic</CText>
+      <CText as="u">Underline</CText>
+      <CText as="abbr">I18N</CText>
+      <CText as="cite">Citation</CText>
+      <CText as="del">Deleted</CText>
+      <CText as="em">Emphasis</CText>
+      <CText as="ins">Inserted</CText>
+      <CText as="kbd">Ctrl + C</CText>
+      <CText as="mark">Highlighted</CText>
+      <CText as="s">Strikethrough</CText>
+      <CText as="samp">Sample</CText>
+      <CText as="sub">sub</CText>
+      <CText as="sup">sup</CText>
+    </CStack>
+    <CDivider />
+    <CBox my="12">
+      Mode: <code>{{ chakraColorMode }}</code>
+    </CBox>
   </CBox>
 </template>
 
 <script>
-import { CBox, CHeading, CDivider } from '@chakra-ui/vue'
+import { CStack, CBox, CHeading, CText, CDivider } from '@chakra-ui/vue'
 export default {
   name: 'DashboardParagraph',
   components: {
+    CStack,
     CBox,
     CHeading,
+    CText,
     CDivider,
   },
   layout: 'dashboard',
